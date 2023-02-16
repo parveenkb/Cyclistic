0409aecf02fec94c9e86f2f36df6a72f00b03601
parveenkb <52286967+parveenkb@users.noreply.github.com>
2023-02-16 14:11:19 -0500

    Kaggle Notebook | Cyclist | Version 9

--- cyclist.r
+++ cyclist.r
@@ -93,15 +93,83 @@ df_clear_data<- delete_Rows_with_zero_mins
 
 Show total number of rider per weekday
 
-```{r pressure}
-# plot Number of rides per day
-
+```{r rides per day cassual and member}
+# plot Number of rides per day for cassual and member users
 ggplot(data=df_clear_data,aes(x= day_of_week))+
   geom_bar(mapping= aes (fill=rideable_type))+
 stat_count(geom="text",aes(label=after_stat(count)),size= 2.5,position= position_stack(vjust=1.1))+
-labs(title="Number of rides per week day with different bike types", x="Day of Week",y="Total Riders",fill="Bike Type")+
-  scale_x_discrete(limits= c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
-
+labs(title="Number of rides per day for both type members", x="Day of Week",y="Total Riders",fill="Bike Type")+
+scale_x_discrete(limits= c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
 ```
 
 Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+```{ r}
+# split Number of rides for casual members and annaual members
+ggplot(data= df_clear_data,aes(x= day_of_week))+
+geom_bar(mapping =aes(fill= rideable_type))+
+stat_count(geom="text",aes(label=..count..),size= 2.5,position = position_stack(vjust=1.1))+
+labs(tittle= "Number of rides per day",x= "Day of Week",y="Total Rides",fill ="Bike Type")+
+facet_grid(~member_casual)+
+scale_y_continuous(labels= comma)+
+scale_x_discrete(limits= c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))+
+theme(axis.text.x = element_text(angle=90))
+```
+```{r}
+# Calculate number of rides per month
+ggplot(data=df_clear_data,aes(x= month.abb[month]))+
+geom_bar(mapping= aes(fill=rideable_type))+
+stat_count(geom="text",aes(label=..count..),size= 2.5,position = position_stack(vjust=1.3))+
+labs(tittle= "Number of rides per month",x= "Months",y = "Total Rides",fill= "Bike Type")+
+facet_grid(~member_casual)+
+scale_y_continuous(labels = comma)+
+scale_x_discrete(limits= c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))
+```
+```{r}
+# we can set limits to order the values on x axis.
+# Calculate the ride duration of the casual user per month
+
+monthly_ride_duration_casual_member<- df_clear_data %>%
+filter(member_casual=="casual")%>%
+group_by(month)%>%
+summarise(duration= sum(ride_length))
+
+ggplot(data= monthly_ride_duration_casual_member)+
+geom_col(aes(x= month,y=duration),fill= "green")+
+labs(title="Monthly Casual Rider Duration", y = "Ride duration in mins",x="Month", fill= "Bike Type")+
+scale_y_continuous(labels = comma)+
+scale_x_discrete(limits= c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))+
+theme_classic()
+```
+```{r}
+# Weekly cassual ride duration
+weekly_ride_duration_casual_member <- df_clear_data%>%
+filter(member_casual=="casual")%>%
+group_by(day_of_week)%>%
+summarise(duration =sum(ride_length))
+```
+```{r}
+# plot graph
+ggplot(data= weekly_ride_duration_casual_member )+
+geom_col(aes(x= day_of_week,y=duration),fill= "light blue")+
+#geom_text(aes(label= duration,x= day_of_week,y= duration), position = position_dodge(width = 0.9), vjust = -0.5)+
+labs(title="Weekly ride duration for casual members",x="Day of week", y = "Ride duration in mins")+
+scale_y_continuous(labels= comma)+
+scale_x_discrete(limits =  c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))+
+theme_classic()
+
+```
+```{r}
+# which type of bike used in July by casual users
+# filter data for casual user from July month
+
+# Popular start stations for member types
+df_routes_taken <- df_clear_data%>% 
+count(member_casual, start_station_name, sort = TRUE) %>% 
+filter(start_station_name!= "NA") %>%
+top_n(10)
+ggplot(df_routes_taken) +
+geom_bar(mapping = aes(y = start_station_name, x = n, fill = member_casual), stat = "identity") + 
+theme_classic() + 
+scale_x_continuous(label=comma)+
+labs(x = "No. of trips started", y = "start station name")
+```
